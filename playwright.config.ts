fa73294a90b0fa9d0a43c2e716650f29f73d3520
jill64 <intents.turrets0h@icloud.com>
2023-09-04 21:36:29 +0900

    fix: test config
    omit old auth

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,9 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   use: {
     baseURL: 'https://dev.sentry-sveltekit-cloudflare.pages.dev'
   },
   testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-  projects: [
-    // Setup project
-    {
-      name: 'setup',
-      testMatch: /.*\.setup\.ts/
-    },
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        // Use prepared auth state.
-        storageState: 'playwright/.auth/user.json'
-      },
-      dependencies: ['setup']
-    }
-  ]
+  testMatch: /(.+\.)?(test|spec)\.[jt]s/
 })
