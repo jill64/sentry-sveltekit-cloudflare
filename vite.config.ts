c98ca91bd1e5378b2d377159b97ad3c06a558f34
jill64 <intents.turrets0h@icloud.com>
2023-09-04 10:09:02 +0900

    add config

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    sourcemap: true
+  },
+  plugins: [
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }),
+    sveltekit()
+  ]
+})
