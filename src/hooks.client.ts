ebfa8795bb190c387eb72bf0d93fe8ae98c86b2c
jill64 <intents.turrets0h@icloud.com>
2023-09-05 13:47:14 +0900

    add toast to demo page

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,8 +1,12 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { clientInit } from '$lib/clientInit.js'
+import { toast } from 'svelte-french-toast'
 
 const onError = clientInit(PUBLIC_SENTRY_DSN)
 
 export const handleError = onError((e) => {
   console.error(e)
+  if (e.error instanceof Error) {
+    toast.error(e.error.message)
+  }
 })
