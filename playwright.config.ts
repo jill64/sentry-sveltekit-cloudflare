dfa4f14128776e6d0f9d45f81dad2fbb1e39a098
jill64 <intents.turrets0h@icloud.com>
2023-10-26 17:47:56 +0900

    update root configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,32 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   use: {
     baseURL: 'https://dev.sentry-sveltekit-cloudflare.pages.dev'
   },
   testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
   fullyParallel: true,
-  workers: '100%'
+  workers: '100%',
+  projects: [
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome']
+    },
+    {
+      name: 'firefox',
+      use: devices['Desktop Firefox']
+    },
+    {
+      name: 'webkit',
+      use: devices['Desktop Safari']
+    },
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5']
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12']
+    }
+  ]
 })
