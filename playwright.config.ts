90ef9c3518e63bd3f2012411adcb7c77533c8816
jill64 <intents.turrets0h@icloud.com>
2023-12-01 21:35:22 +0900

    chore: refactor ci config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,38 +1,12 @@
-import { defineConfig, devices } from '@playwright/test'
+import { extendsConfig } from '@jill64/playwright-config'
 
 const ref_name = process.env.GITHUB_REF_NAME
 
-export default defineConfig({
+export default extendsConfig({
   use: {
     baseURL:
       ref_name === 'main'
         ? 'https://sentry-sveltekit-cloudflare.pages.dev'
         : `https://${ref_name}.sentry-sveltekit-cloudflare.pages.dev`
-  },
-  testDir: 'tests',
-  fullyParallel: true,
-  workers: '100%',
-  retries: process.env.CI ? 2 : 0,
-  projects: [
-    {
-      name: 'chromium',
-      use: devices['Desktop Chrome']
-    },
-    {
-      name: 'firefox',
-      use: devices['Desktop Firefox']
-    },
-    {
-      name: 'webkit',
-      use: devices['Desktop Safari']
-    },
-    {
-      name: 'Mobile Chrome',
-      use: devices['Pixel 5']
-    },
-    {
-      name: 'Mobile Safari',
-      use: devices['iPhone 12']
-    }
-  ]
+  }
 })
