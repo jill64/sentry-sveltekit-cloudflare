9e624ea22c342d1ac6d374fc2b24694caccfd839
jill64 <intents.turrets0h@icloud.com>
2023-12-08 01:44:06 +0900

    Update playwright.config.ts to use branchPreview() function

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,20 +1,3 @@
-import { extendsConfig } from '@jill64/playwright-config'
+import { branchPreview, extendsConfig } from '@jill64/playwright-config'
 
-const ref_name = process.env.GITHUB_REF_NAME?.replaceAll('/', '-')
-  .replace('.', '-')
-  .slice(0, 28)
-
-export default extendsConfig(
-  ref_name === 'main'
-    ? {
-        use: {
-          baseURL: 'https://sentry-sveltekit-cloudflare.pages.dev'
-        }
-      }
-    : {
-        webServer: {
-          command: 'npm run preview',
-          port: 4173
-        }
-      }
-)
+export default extendsConfig(branchPreview('cloudflare'))
