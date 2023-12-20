a84c6ce0ba971525e2ac207e06a2bffef4638764
jill64 <intents.turrets0h@icloud.com>
2023-12-20 10:54:59 +0900

    merge server makeHandler

--- src/server/init.ts
+++ src/server/init.ts
@@ -1,6 +1,13 @@
-import { makeHandler } from './makeHandler.js'
+import { getCurrentHub, runWithAsyncContext } from '@sentry/core'
+import type { Handle, HandleServerError, RequestEvent } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
+import type { Captured } from '../common/types/Captured.js'
+import { initSentry } from './initSentry.js'
+import { instrumentHandle } from './instrumentHandle.js'
 import { HandleWrappers } from './types/HandleWrappers.js'
 import { InitOptions } from './types/InitOptions.js'
+import { defaultErrorHandler } from './util/defaultErrorHandler.js'
+import { isNotFoundError } from './util/isNotFoundError.js'
 
 export const init = (
   /**
@@ -12,4 +19,43 @@ export const init = (
    * Server Init Options
    */
   options?: InitOptions
-): HandleWrappers => makeHandler(dsn, options)
+): HandleWrappers => {
+  const { handleOptions, toucanOptions } = options ?? {}
+
+  const init = (event: RequestEvent) =>
+    initSentry(event.request, {
+      tracesSampleRate: 1.0,
+      dsn,
+      ...toucanOptions
+    })
+
+  const sentryHandle: Handle = (input) => {
+    const sentryHandleOption = { input, init, handleOptions }
+
+    return getCurrentHub().getScope().getSpan()
+      ? instrumentHandle(sentryHandleOption)
+      : runWithAsyncContext(() => instrumentHandle(sentryHandleOption))
+  }
+
+  const onHandle = (handle?: Handle) =>
+    handle ? sequence(sentryHandle, handle) : sentryHandle
+
+  const onError: Captured<HandleServerError> =
+    (handleError = defaultErrorHandler) =>
+    (input) => {
+      if (isNotFoundError(input)) {
+        return handleError(input)
+      }
+
+      const Sentry = init(input.event)
+
+      const result = Sentry.captureException(input.error)
+
+      return handleError(input, result)
+    }
+
+  return {
+    onHandle,
+    onError
+  }
+}
