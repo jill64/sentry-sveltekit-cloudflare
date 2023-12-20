16f70c8fa3161a3cb8128cc43791ec97f39be922
jill64 <intents.turrets0h@icloud.com>
2023-12-20 11:28:49 +0900

    refactor instrumentHandle according to @sentry/sveltekit

--- src/server/instrumentHandle.ts
+++ src/server/instrumentHandle.ts
@@ -1,73 +1,75 @@
-import { getCurrentHub, trace } from '@sentry/core'
+import { getCurrentScope, startSpan } from '@sentry/core'
 import { objectify } from '@sentry/utils'
-import type { Handle, RequestEvent } from '@sveltejs/kit'
+import type { RequestEvent } from '@sveltejs/kit'
+import { Handle } from '@sveltejs/kit'
+import { isHttpError, isRedirect } from '../common/utils.js'
 import { getTracePropagationData } from './getTracePropagationData.js'
 import { initSentry } from './initSentry.js'
 import { transformPageChunk } from './transformPageChunk.js'
-import { isHttpError, isRedirect } from '../common/utils.js'
 import { SentryHandleOptions } from './types/SentryHandleOptions.js'
 
-export const instrumentHandle = (arg: {
+export const instrumentHandle = async ({
+  input: { event, resolve },
+  init,
+  handleOptions: options
+}: {
   input: Parameters<Handle>[0]
   init: (event: RequestEvent) => ReturnType<typeof initSentry>
   handleOptions?: SentryHandleOptions
 }) => {
-  const { input, handleOptions, init } = arg
-  const { event, resolve } = input
-
-  const instrumentOptions = {
-    handleUnknownRoutes: false,
-    ...handleOptions
-  }
-
-  if (!event.route?.id && !instrumentOptions.handleUnknownRoutes) {
+  if (!event.route?.id && !options?.handleUnknownRoutes) {
     return resolve(event)
   }
-
-  const Sentry = init(input.event)
 
   const { dynamicSamplingContext, traceparentData, propagationContext } =
     getTracePropagationData(event)
 
-  getCurrentHub().getScope().setPropagationContext(propagationContext)
+  getCurrentScope().setPropagationContext(propagationContext)
 
-  return trace(
-    {
-      op: 'http.server',
-      origin: 'auto.http.sveltekit',
-      name: `${event.request.method} ${event.route?.id || event.url.pathname}`,
-      status: 'ok',
-      ...traceparentData,
-      metadata: {
-        source: event.route?.id ? 'route' : 'url',
-        dynamicSamplingContext:
-          traceparentData && !dynamicSamplingContext
-            ? {}
-            : dynamicSamplingContext
+  try {
+    const resolveResult = await startSpan(
+      {
+        op: 'http.server',
+        origin: 'auto.http.sveltekit',
+        name: `${event.request.method} ${
+          event.route?.id ?? event.url.pathname
+        }`,
+        status: 'ok',
+        ...traceparentData,
+        metadata: {
+          source: event.route?.id ? 'route' : 'url',
+          dynamicSamplingContext:
+            traceparentData && !dynamicSamplingContext
+              ? {}
+              : dynamicSamplingContext
+        }
+      },
+      async (span) => {
+        const res = await resolve(event, { transformPageChunk })
+        if (span) {
+          span.setHttpStatus(res.status)
+        }
+        return res
       }
-    },
-    async (span) => {
-      const res = await resolve(event, { transformPageChunk })
-      if (span) {
-        span.setHttpStatus(res.status)
-      }
-      return res
-    },
-    (e) => {
-      const objectifiedErr = objectify(e)
+    )
+    return resolveResult
+  } catch (e) {
+    const objectifiedErr = objectify(e)
 
-      if (
-        isRedirect(objectifiedErr) ||
-        (isHttpError(objectifiedErr) &&
-          objectifiedErr.status < 500 &&
-          objectifiedErr.status >= 400)
-      ) {
-        return objectifiedErr
-      }
-
-      Sentry.captureException(objectifiedErr)
-
-      return objectifiedErr
+    // similarly to the `load` function, we don't want to capture 4xx errors or redirects
+    if (
+      isRedirect(objectifiedErr) ||
+      (isHttpError(objectifiedErr) &&
+        objectifiedErr.status < 500 &&
+        objectifiedErr.status >= 400)
+    ) {
+      throw e
     }
-  )
+
+    const Sentry = init(event)
+
+    Sentry.captureException(objectifiedErr)
+
+    throw e
+  }
 }
