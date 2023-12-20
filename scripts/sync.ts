7a5aff264551b80129237079b9411c7b6b83c511
jill64 <intents.turrets0h@icloud.com>
2023-12-20 11:37:50 +0900

    fix: npm sync

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,11 +1,30 @@
-import { cp, rm } from 'node:fs/promises'
+import { exec } from 'node:child_process'
+import { cp, readFile, rm, writeFile } from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { promisify } from 'node:util'
 
-const dest = 'src/sentry-sveltekit'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const source = path.join(__dirname, '../node_modules/@sentry/sveltekit')
+const dest = path.join(__dirname, '../src/sentry-sveltekit')
 
 await rm(dest, { recursive: true, force: true })
-
-await cp('node_modules/@sentry/sveltekit', dest, {
+await cp(source, dest, {
   recursive: true,
   dereference: true,
   force: true
 })
+
+// remove version from package.json to avoid conflicts
+const filepath = path.join(dest, 'package.json')
+const packageJson = await readFile(filepath, 'utf-8')
+const json = JSON.parse(packageJson)
+await writeFile(
+  filepath,
+  JSON.stringify({ ...json, version: undefined }, null, 2)
+)
+
+// Refresh workspace dependencies
+await promisify(exec)('pnpm i')
